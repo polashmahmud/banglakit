715d9716920309583682c02a835eabc6de2072cc
Polash Mahmud <polashmahmud@gmail.com>
2025-10-28 18:38:56 +0600

    feat: update VitePress configuration and enhance documentation with Bengali localization

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,19 +2,20 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Bangla Kit",
-  description: "A VitePress Site",
+  ignoreDeadLinks: true,
+  title: "বাংলা কিট",
+  description: "একটি পূর্ণাঙ্গ বাংলা ডেভেলপমেন্ট কিট।",
   base: "/banglakit/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'হোম পেজ', link: '/' },
+      { text: 'ডকুমেন্টেশন', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'ডকুমেন্টেশন',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
@@ -23,7 +24,41 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      { icon: "github", link: "https://github.com/polashmahmud/php" },
+      {
+        icon: "facebook",
+        link: "https://www.facebook.com/learnwithpolashmahmud",
+      },
+      { icon: "youtube", link: "https://www.youtube.com/polashmahmud4" },
+      { icon: "linkedin", link: "https://www.linkedin.com/in/polashmahmud4/" },
+      { icon: "discord", link: "https://discord.gg/mktPP7n9xp" },
+      { icon: "x", link: "https://twitter.com/polashmahmud4" },
+    ],
+
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2025-present Polash Mahmud",
+    },
+
+    search: {
+      provider: "local",
+    },
+  },
+  head: [
+    [
+      'link',
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+    ],
+    [
+      'link',
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+    ],
+    [
+      'link',
+      {
+        href: 'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap',
+        rel: 'stylesheet'
+      }
+    ],
+  ],
 })
